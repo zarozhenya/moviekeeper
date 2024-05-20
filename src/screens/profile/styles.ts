87d26968962d0a404635c621760aeca1c9f45ebe
Yevhen <eug.z2004@gmail.com>
2024-05-20 12:12:59 +0300

    added profile screen

---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'space-between',
+  },
+});
